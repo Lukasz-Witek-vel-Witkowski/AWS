17487993a49929709116c7a4585725e6faae73fa
Lukasz_Witek_vel_Witkowski <uwlwitkowski@gmail.com>
2019-04-05 20:49:35 +0200

    Łukasz Witek vel Witkowski
    commit uzupełniający

--- AWS/File.h
+++ AWS/File.h
@@ -1,7 +1,6 @@
 #pragma once
 #ifndef FILE_HPP
 #define FILLE_HPP
-//#include "Section.h"
 #include "Segment.h"
 class File
 {
@@ -9,18 +8,13 @@ public:
 	File();
 	void setNameFile(std::string name);
 	void setOutputFolder(std::string name);
-	void setOutPutFile(std::string name);
+	void setOutPutFiles(std::string name);
 	void setPath(std::string path);
-	//void addSegment(_SegmentF& s);
-	//_SegmentF& getSegment(unsigned int value);
 	void loadFile();
 	_SegmentF& getTemp();
-	//bool eof(std::istream& file);
-	//unsigned int getiterator();
 	void saveFile();
 	void SetOutPutFileCanal(std::string data);
 	void setIterator(int Canal);
-	//void updateCanal();
 	int getCanal();
 	void clear();
 	~File();
@@ -29,10 +23,9 @@ private:
 	int canal;
 	int iteratorSegments;
 	int iteratorChanel;
-	//std::vector<_SegmentF> vSegment; //tablica dynamiczna
 	std::string nameFile;
 	std::string path;
-	std::string outPutFile;
+	std::string outPutFiles;
 	std::string outputFolder;
 };
 std::istream& operator>>(std::istream& file, File& f);
